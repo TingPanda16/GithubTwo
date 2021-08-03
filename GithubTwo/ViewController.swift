b6fb4994f87e4edfd6eae7bdd2a70eba143ad173
Ting Ting <lipanda1605a@gmail.com>
2021-08-03 11:03:09 -0400

    Initial Commit

---
+++ GithubTwo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubTwo
+//
+//  Created by Ting Ting Li on 8/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
